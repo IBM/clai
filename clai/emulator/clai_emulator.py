f8a8646766650eb02e500364848e31ce78826556
Jorge Juan Barroso Carmona <jorge@karumi.com>
2020-02-25 13:13:25 +0100

    Redesign the colors and display more message info

--- clai/emulator/clai_emulator.py
+++ clai/emulator/clai_emulator.py
@@ -6,14 +6,17 @@
 #
 
 import os
+import re
 import tkinter as tk
 from tkinter import ttk, messagebox
+from tkinter.font import Font
 from typing import List
 
 from clai.emulator.toggled_frame import ToggledFrame
 from clai.emulator.emulator_presenter import EmulatorPresenter
 
 # pylint: disable=too-many-instance-attributes,protected-access,attribute-defined-outside-init,too-many-public-methods
+from clai.server.command_message import Action, ProcessesValues, NetworkValues, Process
 from clai.server.command_runner.clai_last_info_command_runner import InfoDebug
 
 
@@ -23,16 +26,24 @@ class ClaiEmulator:
         self.presenter = EmulatorPresenter(self.on_skills_ready, self.on_server_running, self.on_server_stopped)
 
     def launch(self):
-        root = tk.Tk()
-        root.geometry("900x600")
-        self.add_toolbar(root)
-        self.add_send_command_box(root)
-        self.add_list_commands(root)
+        self.root = tk.Tk()
+        self.root.wait_visibility(self.root)
+        self.title_font = Font(size=16, weight='bold')
+        self.bold_font = Font(weight='bold')
 
-        root.protocol("WM_DELETE_WINDOW", lambda root=root: self.on_closing(root))
-        root.createcommand('exit', lambda root=root: self.on_closing(root))
+        self.root.geometry("900x600")
 
-        root.mainloop()
+        style = ttk.Style(self.root)
+        style.configure("TLable", bg="black")
+
+        self.add_toolbar(self.root)
+        self.add_send_command_box(self.root)
+        self.add_list_commands(self.root)
+
+        self.root.protocol("WM_DELETE_WINDOW", lambda root=self.root: self.on_closing(root))
+        self.root.createcommand('exit', lambda root=self.root: self.on_closing(root))
+
+        self.root.mainloop()
 
     def on_closing(self, root):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
@@ -67,55 +78,66 @@ class ClaiEmulator:
         skill_name = self.extract_skill_name(self.selected_skills.get())[0]
         self.presenter.select_skill(skill_name)
 
-    def add_row(self, response: str, info: InfoDebug):
+    def add_detail_label(self, parent: ttk.Frame, title: str, text_value: str, side: str):
+        row = ttk.Frame(parent)
+        title_label = ttk.Label(parent, text=title, font=self.bold_font, anchor='n', padding=(10, 0, 0, 0))
+        title_label.pack(side=side, fill=tk.BOTH)
+        row_value = ttk.Label(parent, text=text_value, wraplength=250, anchor='nw')
 
+        if side == tk.LEFT:
+            row_value.pack(side=side, fill=tk.BOTH, expand=True)
+        else:
+            row_value.pack(side=side, fill=tk.BOTH, before=title_label)
+
+        row.pack(side=side, fill=tk.BOTH, expand=True)
+
+    def add_row(self, response: str, info: InfoDebug):
         toggled_frame = ToggledFrame(self.frame, text=response, relief=tk.RAISED, borderwidth=1)
         toggled_frame.pack(fill="x", expand=1, pady=2, padx=2, anchor="n")
 
         first_row = ttk.Frame(toggled_frame.sub_frame)
         first_row.pack(fill="x", expand=True)
-        ttk.Label(first_row, text=f"Original: {info.command}").pack(side=tk.LEFT, padx=10)
-        ttk.Label(first_row, text=f"Id: {info.command_id}").pack(side=tk.RIGHT, padx=10)
+        self.add_detail_label(first_row, "Original:", info.command, side=tk.LEFT)
+        self.add_detail_label(first_row, "Id:", info.command_id, side=tk.RIGHT)
 
         second_row = ttk.Frame(toggled_frame.sub_frame)
         second_row.pack(fill="x", expand=True)
-        ttk.Label(second_row, text=f'Description: {self.remove_emoji(info.action_suggested.description)}') \
-            .pack(side=tk.LEFT, padx=10)
-        ttk.Label(second_row,
-                  text=f"Confidence:{info.action_suggested.confidence} Force: {info.action_suggested.execute}") \
-            .pack(side=tk.RIGHT, padx=10)
+        self.add_detail_label(second_row, 'Description:', self.remove_emoji(info.action_suggested.description), tk.LEFT)
+        self.add_detail_label(second_row, 'Confidence:', f'{info.action_suggested.confidence}', tk.RIGHT)
+        self.add_detail_label(second_row, 'Force:', f'{info.action_suggested.execute}', tk.RIGHT)
 
         third_row = ttk.Frame(toggled_frame.sub_frame)
         third_row.pack(fill="x", expand=True)
-        ttk.Label(third_row, text=f'Agent: {info.action_suggested.agent_owner}') \
-            .pack(side=tk.LEFT, padx=10)
-        ttk.Label(third_row,
-                  text=f"Sugestion:{info.action_suggested.suggested_command}  Applied: {info.already_processed}") \
-            .pack(side=tk.RIGHT, padx=10)
+        self.add_detail_label(third_row, 'Agent:', text_value=info.action_suggested.agent_owner, side=tk.LEFT)
+        self.add_detail_label(third_row, 'Sugestion:', text_value=info.action_suggested.suggested_command,
+                              side=tk.RIGHT)
+        self.add_detail_label(third_row, 'Applied:', text_value=f'{info.already_processed}', side=tk.RIGHT)
 
         fourth_row = ttk.Frame(toggled_frame.sub_frame)
         fourth_row.pack(fill="x", expand=True)
         process_text = ','.join(list(map(lambda process: process.name, info.processes.last_processes)))
-        ttk.Label(fourth_row, text=f'Processes: {process_text}') \
-            .pack(side=tk.LEFT, padx=10)
+        self.add_detail_label(fourth_row, 'Processes:', text_value=f'{process_text}', side=tk.LEFT)
 
-        ttk.Label(toggled_frame.sub_frame, text=f'Post execution:').pack(side=tk.LEFT, padx=10)
+        post_title_frame = ttk.Frame(toggled_frame.sub_frame)
+        post_title_frame.pack(fill='x', expand=True)
+        ttk.Label(post_title_frame, text=f'Post execution', anchor='center', font=self.title_font). \
+            pack(side=tk.LEFT, padx=10, fill='x', expand=True)
 
-        third_row = ttk.Frame(toggled_frame.sub_frame)
-        third_row.pack(fill="x", expand=True)
+        fifth_row = ttk.Frame(toggled_frame.sub_frame)
+        fifth_row.pack(fill="x", expand=True)
         post_description = ""
         post_confidence = 0
         if info.action_post_suggested:
             post_description = info.action_post_suggested.description
             post_confidence = info.action_post_suggested.confidence
-        ttk.Label(third_row, text=f'Description: {self.remove_emoji(post_description)}') \
-            .pack(side=tk.LEFT, padx=10)
-        ttk.Label(third_row, text=f"Confidence: {post_confidence}") \
-            .pack(side=tk.RIGHT, padx=10)
+
+        self.add_detail_label(fifth_row, 'Description:', text_value=f'{self.remove_emoji(post_description)}',
+                              side=tk.LEFT)
+        self.add_detail_label(fifth_row, 'Confidence:', text_value=f'{post_confidence}', side=tk.RIGHT)
 
     def add_send_command_box(self, root):
         button_bar_frame = tk.Frame(root, bd=1, relief=tk.RAISED)
-        send_button = tk.Button(button_bar_frame, padx=10, text='Send', command=self.on_send_click)
+        send_button = tk.Button(button_bar_frame, padx=10, text=u"\u2713", command=self.on_send_click)
         send_button.pack(side=tk.RIGHT, padx=5)
         self.text_input = tk.StringVar()
         send_edit_text = tk.Entry(button_bar_frame, textvariable=self.text_input)
@@ -133,7 +155,7 @@ class ClaiEmulator:
         toolbar.pack(side=tk.TOP, fill=tk.X)
 
     def add_skills_selector(self, root, toolbar):
-        label = ttk.Label(toolbar, text="Skills")
+        label = ttk.Label(toolbar, text=u'Skills')
         label.pack(side=tk.LEFT, padx=2)
 
         self.selected_skills = tk.StringVar(root)
@@ -168,8 +190,16 @@ class ClaiEmulator:
     def on_send_click(self):
         self.send_command(self.text_input.get())
 
+    @staticmethod
+    def clean_message(text: str):
+        text = text[text.find(']#'):]
+        text = re.sub('[[0-9;]*m', '', text)
+        return text[:text.find(']0;') - 3]
+
     def send_command(self, command):
         stdout, info = self.presenter.send_message(command)
+        stdout = self.clean_message(stdout)
+
         self.add_row(stdout, info)
         self.text_input.set("")
 
