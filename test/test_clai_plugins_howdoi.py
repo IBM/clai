7f2d737373fbf31e004bb0da59e924b96d4f5caf
Kshitij Fadnis <cool.kshij@gmail.com>
2020-03-12 05:04:09 -0400

    enabled question based invocation for howdoi skill (#38)
    * enabled question based invocation
    
    * fix the tests
    
    Co-authored-by: Kshitij P. Fadnis <kpfadnis@us.ibm.com>
    Co-authored-by: Jorge Juan Barroso Carmona <jorge@karumi.com>

--- test/test_clai_plugins_howdoi.py
+++ test/test_clai_plugins_howdoi.py
@@ -11,11 +11,38 @@ from clai.server.command_message import State
 from clai.server.plugins.howdoi.howdoi import HowDoIAgent
 
 
+@unittest.skip("Only for local testing")
 class SearchAgentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         _agent = HowDoIAgent()
         cls.agent = _agent
+
+    @unittest.skip("Only for local testing")
+    def test_get_next_action_pwd_without_question(self):
+        self.agent.init_agent()
+
+        state = State(user_name='tester', command_id='0', command="pwd")
+        action = self.agent.get_next_action(state=state)
+        print("Input: {}".format(state.command))
+        print("===========================")
+        print("Response: {}".format(action.suggested_command))
+        print("===========================")
+        print("Explanation: {}".format(action.description))
+        self.assertEqual('pwd', action.suggested_command)
+
+    @unittest.skip("Only for local testing")
+    def test_get_next_action_pwd_with_question(self):
+        self.agent.init_agent()
+
+        state = State(user_name='tester', command_id='0', command="What is pwd?")
+        action = self.agent.get_next_action(state=state)
+        print("Input: {}".format(state.command))
+        print("===========================")
+        print("Response: {}".format(action.suggested_command))
+        print("===========================")
+        print("Explanation: {}".format(action.description))
+        self.assertEqual('man pwd', action.suggested_command)
 
     @unittest.skip("Only for local testing")
     def test_get_next_action_sudo(self):
