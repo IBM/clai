ad855ce12eb3d9e1c9da6dbce4e04b5a6548b45e
Dan FitzGerald <danfitz@us.ibm.com>
2020-03-31 16:48:43 -0400

    Add CLAI_DOCKER_CONTAINER_NAME tuneable to RunDockerImage.sh

--- RunDockerImage.sh
+++ RunDockerImage.sh
@@ -16,6 +16,10 @@
 # defined, uses the default flag value 'claiplayground' for the docker image.
 image_name=${CLAI_DOCKER_IMAGE_NAME-"claiplayground"}
 
+# Looks for an environment var named CLAI_DOCKER_CONTAINER_NAME. If not
+# defined, uses the default flag value 'CLAIBotPlayground' for the container.
+image_name=${CLAI_DOCKER_CONTAINER_NAME-"CLAIBotPlayground"}
+
 # Controls where CLAI would store internal states for persistence
 DefaultBaseDir="${HOME}/.clai"
 HostBaseDir="${CLAI_BASEDIR:-${DefaultBaseDir}}"
@@ -28,14 +32,14 @@ ContainerBaseDir="/root/.clai"
 # Forward the ports to the localhost so we can SSH            (-P)
 # Run docker with 2GB of memory                               (-m 2GB)
 # Mount a host directory to the container directory           (-v ${HostBaseDir}:${ContainerBaseDir})
-# Provide a handy human readable name for the container       (--name CLAIPlayground)
+# Provide a handy human readable name for the container       (--name ${CLAI_DOCKER_CONTAINER_NAME})
 
 docker run --privileged							  	 \
            -t -d                                      \
            -P                                         \
            -m 2g                                      \
            -v ${HostBaseDir}:${ContainerBaseDir}      \
-           --name CLAIBotPlayground					  \
+           --name ${CLAI_DOCKER_CONTAINER_NAME}       \
 	   $CLAI_DOCKER_IMAGE_NAME
 
 echo 'User for ssh is root and the default pass Bashpass'
