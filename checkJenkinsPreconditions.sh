6dda071dd18cb63437083b4b646242c498e6d607
Dan FitzGerald <danfitz@us.ibm.com>
2020-04-01 12:23:47 -0400

    checkJenkinsPreconditions.sh now detects when called in container

--- checkJenkinsPreconditions.sh
+++ checkJenkinsPreconditions.sh
@@ -19,20 +19,50 @@
 #         * Initial creation
 #
 
-echo "This is a test"
-
-# Make sure that "python3" is an alias for python
-isPython3There=$(command -v python3)
-if [ -z $isPython3There ]; then
-    echo "no bin or alias named 'python3'; unable to continue"
-    exit 1
-fi
-
 # Make sure that we have sshpass installed
 isSshpassThere=$(command -v sshpass)
 if [ -z $isSshpassThere ]; then
     echo "no bin or alias named 'sshpass'; unable to continue"
     exit 1
+fi
+
+# The following checks will ONLY occur if the __JENKINSCHECK_IS_CONTAINER
+# environment variable was set at script invocation
+if [ -n "$__JENKINSCHECK_IS_CONTAINER" ]; then
+    
+    # Make sure that "python3" is an alias for python
+    isPython3There=$(command -v python3)
+    if [ -z $isPython3There ]; then
+        echo "no bin or alias named 'python3'; unable to continue"
+        exit 1
+    fi
+
+    # Make sure we have pip installed
+    isPipThere=$(command -v pip)
+    if [ -z $isPipThere ]; then
+        echo "pip is not installed; unable to continue"
+        exit 1
+    fi
+    
+    # Make sure that we have pytest installed
+    isPytestThere=$(pip freeze | grep pytest | wc -l)
+    if [ $isPytestThere -eq 0 ]; then
+        python3 -m pip install pytest
+        if [ $? -ne 0 ]; then
+            echo "pytest can not be installed; unable to continue"
+            exit 1
+        fi
+    fi
+    
+    # Make sure that we have pytest_docker_tools installed
+    isPytestDockerToolsThere=$(pip freeze | grep pytest_docker_tools | wc -l)
+    if [ $isPytestDockerToolsThere -eq 0 ]; then
+        python3 -m pip install pytest_docker_tools
+        if [ $? -ne 0 ]; then
+            echo "pytest_docker_tools can not be installed; unable to continue"
+            exit 1
+        fi
+    fi
 fi
 
 # All's well
