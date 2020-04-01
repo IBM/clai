a75caa3e634b226d194692a6bd090370c06d9b42
Dan FitzGerald <danfitz@us.ibm.com>
2020-04-01 16:44:09 -0400

    Add jenkinsbuild build-arg to Dockerfile and a way to specify it

--- BuildDockerImage.sh
+++ BuildDockerImage.sh
@@ -16,12 +16,16 @@ flags=${DOCKER_BUILD_FLAGS-"--no-cache"}
 # defined, uses the default flag value 'claiplayground' for the docker image.
 image_name=${CLAI_DOCKER_IMAGE_NAME-"claiplayground"}
 
+# Looks for an environment var named CLAI_DOCKER_BUILD_ARGS. If not
+# defined, defaults to the empty string.
+buildargs=${CLAI_DOCKER_BUILD_ARGS-""}
+
 echo "==============================================================="
 echo ""
 echo " Phase 1: Building CLAI Container $flags"
 echo ""
 echo "==============================================================="
-time docker build -f Dockerfile.CLAI -t $image_name . $flags
+time docker build -f Dockerfile.CLAI -t $image_name $buildargs . $flags
 if [ $? -ne 0 ]
 then
         echo "Failed to build CLAI Playground Container. Aborting Build."
--- Jenkinsfile
+++ Jenkinsfile
@@ -41,8 +41,9 @@ pipeline {
                     }
                     else{
                         sh """
-                            sudo DOCKER_BUILD_FLAGS=""\
+                            sudo DOCKER_BUILD_FLAGS='' \
                             CLAI_DOCKER_IMAGE_NAME=${env.IMAGE_NAME} \
+                            CLAI_DOCKER_BUILD_ARGS='--build-arg jenkinsbuild=true' \
                             ${env.WORKSPACE}/BuildDockerImage.sh
                         """
                     }
