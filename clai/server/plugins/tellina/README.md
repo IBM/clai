3fc20287840f5508ce68bed416c4e1c47a60bf52
Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>
2020-04-09 20:32:06 -0400

    tellina skill

---
+++ clai/server/plugins/tellina/README.md
@@ -0,0 +1,10 @@
+# Tellina
+
+`NLP` `Support`
+
+
+## [xkcd](https://uni.xkcd.com/)
+
+I don't know what's worse--the fact that after 15 years of using tar I still can't keep the flags straight, or that after 15 years of technological advancement I'm still mucking with tar flags that were 15 years old when I started.  
+
+![alt text](https://imgs.xkcd.com/comics/tar.png "I don't know what's worse--the fact that after 15 years of using tar I still can't keep the flags straight, or that after 15 years of technological advancement I'm still mucking with tar flags that were 15 years old when I started.")
---
+++ clai/server/plugins/tellina/nlc2cmd.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020 IBM. All Rights Reserved.
+#
+# See LICENSE.txt file in the root directory
+# of this source tree for licensing information.
+#
+
+from clai.server.agent import Agent
+from clai.server.command_message import State, Action, NOOP_COMMAND
+from clai.tools.colorize_console import Colorize
+
+import requests
+
+''' globals '''
+tellina_endpoint = 'https://tellina-server.mybluemix.net/message'
+
+class TELLINA(Agent):
+    def __init__(self):
+        super(TELLINA, self).__init__()
+        self.service = Service()
+
+    def get_next_action(self, state: State) -> Action:
+
+        # user typed in, in natural language
+        command = state.command
+
+        try:
+            response = requests.put(tellina_endpoint, json={'text': command}).json()
+
+            ## tellina endpoint must return a json with
+            ## keys "response" and "confidence"
+
+            response = response['response']
+            confidence = response['confidence']
+
+            return Action(
+                suggested_command=NOOP_COMMAND,
+                execute=True,
+                description=Colorize().info().append(response).to_console(),
+                confidence=confidence)
+
+        except Exception as ex:
+            return [ { "text" : "Method failed with status " + str(ex) }, 0.0 ]
