666086bd60b8c637bef20756db87668f697ab2da
Dan FitzGerald <danfitz@us.ibm.com>
2020-04-21 16:57:29 -0400

    Implement variant searching for 'howdoi' skill

--- clai/server/plugins/helpme/helpme.py
+++ clai/server/plugins/helpme/helpme.py
@@ -57,7 +57,7 @@ class HelpMeAgent(Agent):
 
     def post_execute(self, state: State) -> Action:
 
-        logger.info("=========================== In Helpme Bot:post_execute ===================================")
+        logger.info("==================== In Helpme Bot:post_execute ============================")
         logger.info("State:\n\tCommand: {}\n\tError Code: {}\n\tStderr: {}".format(state.command,
                                                                                    state.result_code,
                                                                                    state.stderr))
