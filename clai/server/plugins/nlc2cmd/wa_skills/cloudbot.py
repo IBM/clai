8668a0bba0213528e8c59bbd41deaa254efc6f20
Dan FitzGerald <danfitz@us.ibm.com>
2020-05-19 10:40:48 -0400

    Copy ISS133 changes from main CLAI GitHub

--- clai/server/plugins/nlc2cmd/wa_skills/cloudbot.py
+++ clai/server/plugins/nlc2cmd/wa_skills/cloudbot.py
@@ -8,10 +8,31 @@
 ''' cloud starter-sheet command handler '''
 
 ''' imports '''
-import requests
+from clai.server.plugins.nlc2cmd.wa_skills.utils import call_wa_skill, get_own_name
 
 ''' globals '''
-wa_endpoint = 'https://ibmcloud-helper.mybluemix.net/message'
+__self = get_own_name(__file__) 
+__intents = { 'add_tag_resource'                 : "resource tag-attach --tag-names TAG --resource-name NAME",
+              'assign_access_create_policy'      : "iam user-policy-create USER_NAME [OPTIONS]",
+              'create_access_group'              : "iam user-policy-create USER_NAME [OPTIONS]",
+              'create_api_key'                   : "iam api-key-create NAME -d DESCRIPTION",
+              'create_org_cloudfoundry_services' : "account org-create ORG_NAME",
+              'create_resource'                  : "resource service-instance-create NAME SERVICE_NAME PLAN_NAME LOCATION",
+              'create_resource_group'            : "resource group create GROUP_NAME",
+              'create_space_services_org'        : "account space-create SPACE_NAME",
+              'install_plugin'                   : "plugin install NAME",
+              'invite_user_to_account'           : "account user-invite USER_EMAIL",
+              'list_plugins'                     : "plugin repo-plugins",
+              'list_services_resource_group'     : "resource service-instances -g GROUP_NAME",
+              'list_tags_account'                : "resource tags",
+              'list_users_in_account'            : "account users (works only for account owners)",
+              'search_ibm_cloud_catalog'         : "catalog search QUERY",
+              'target_cloudfoundry_org'          : "target --cf",
+              'view_service_details'             : "catalog service NAME",
+              'view_usage_costs_month'           : "billing account-usage [-d YYYY-MM]",
+              'login'                            : "login",
+              'help'                             : "help COMMAND",
+              'generic'                          : "help" }
 
 def wa_skill_processor_cloudbot(msg):
 
@@ -22,16 +43,8 @@ def wa_skill_processor_cloudbot(msg):
     if msg.startswith('ibmcloud'):
         return None, 0.0
 
-    try:
-        response = requests.put(wa_endpoint, json={'text': msg}).json()
-
-        if response['result'] == 'success':
-            response = response['response']['output']
-        else:
-            return [ { "text" : response['result'] }, confidence ]
-
-    except Exception as ex:
-        return [ { "text" : "Method failed with status " + str(ex) }, confidence ]
+    response, success = call_wa_skill(msg, __self)
+    if not success: return {"text" : response}, 0.0
 
     # Identify the intent in the user message
     try:
@@ -42,30 +55,10 @@ def wa_skill_processor_cloudbot(msg):
     except IndexError or KeyError:
         intent = "ibmcloud help"
 
-    intents = { 'add_tag_resource': {"text": "Try >> ibmcloud resource tag-attach --tag-names TAG --resource-name NAME"},
-                'assign_access_create_policy': {"text": "Try >> ibmcloud iam user-policy-create USER_NAME [OPTIONS]"},
-                'create_access_group': {"text": "Try >> ibmcloud iam user-policy-create USER_NAME [OPTIONS]"},
-                'create_api_key': {"text": "Try >> ibmcloud iam api-key-create NAME -d DESCRIPTION"},
-                'create_org_cloudfoundry_services': {"text": "Try >> ibmcloud account org-create ORG_NAME"},
-                'create_resource': {"text": "Try >> ibmcloud resource service-instance-create NAME SERVICE_NAME PLAN_NAME LOCATION"},
-                'create_resource_group': {"text": "Try >> ibmcloud resource group create GROUP_NAME"},
-                'create_space_services_org': {"text": "Try >> ibmcloud account space-create SPACE_NAME"},
-                'help': {"text": "Try >> ibmcloud help COMMAND"},
-                'install_plugin': {"text": "Try >> ibmcloud plugin install NAME"},
-                'invite_user_to_account': {"text": "Try >> ibmcloud account user-invite USER_EMAIL"},
-                'list_plugins': {"text": "Try >> ibmcloud plugin repo-plugins"},
-                'list_services_resource_group': {"text": "Try >> ibmcloud resource service-instances -g GROUP_NAME"},
-                'list_tags_account': {"text": "Try >> ibmcloud resource tags"},
-                'list_users_in_account': {"text": "Try >> ibmcloud account users (works only for account owners)"},
-                'login': {"text": "Try >> ibmcloud login"},
-                'search_ibm_cloud_catalog': {"text": "Try >> ibmcloud catalog search QUERY"},
-                'target_cloudfoundry_org': {"text": "Try >> ibmcloud target --cf"},
-                'view_service_details': {"text": "Try >> ibmcloud catalog service NAME"},
-                'view_usage_costs_month': {"text": "Try >> ibmcloud billing account-usage [-d YYYY-MM]"}}
+        intent = "generic"
+        confidence = 0.0
 
-    if intent in intents: data = intents[intent]
-    else: pass
-
+    data = { "text" : "Try >> ibmcloud " + __intents[intent] } 
     return data, confidence
 
 
