57fa4e2a3eb811a37952bc884bdef7e7db6834f0
Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>
2020-05-26 11:00:16 -0400

    standardize zosbot format
    Signed-off-by: Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>

--- clai/server/plugins/nlc2cmd/wa_skills/zosbot.py
+++ clai/server/plugins/nlc2cmd/wa_skills/zosbot.py
@@ -38,34 +38,35 @@ def wa_skill_processor_zosbot(msg):
         else: entities[item['entity']] = [item['value']]
 
     if intent == "bpxmtext":
-        data = {"text" : "Try >> bpxmtext <reasoncode>"}
+        data = {"text" : "Try >> bpxmtext [reasoncode]"}
 
     elif intent == "compile-c-code":
         data = {"text" : "Try >> xlc"}
     
     elif intent == "extattr":
-        data = {"text" : "Try >> extattr [+alps] [-alps] [-Fformat] file ..."}
+        data = {"text" : "Try >> extattr [+alps] [-alps] [-Fformat] [file] ..."}
     
     elif intent == "obrowse":
         data = {"text" : "Try >> obrowse -r xx [file]"}
     
     elif intent == "oedit":
-        data = {"text" : "Try >> oedit [-r xx] [file]"}
+        data = {"text" : "Try >> oedit -r xx [file]"}
     
     elif intent == "oget":
-        data = {"text" : "Try >> OGET 'pathname' mvs_data_set_name(member_name)"}
+        data = {"text" : "Try >> OGET [pathname] mvs_data_set_name(member_name)"}
     
     elif intent == "oput":
-        data = {"text" : "Try >> OPUT mvs_data_set_name(member_name) 'pathname'"}
+        data = {"text" : "Try >> OPUT mvs_data_set_name(member_name) [pathname]"}
     
     elif intent == "oeconsol":
 
-        if 'iplinfo' in entities: data = {"text" : "Try >> oeconsol 'd iplinfo'"}
-        elif 'command' in entities: data = {"text" : "Try >> oeconsol '<command>'"}
-        else: data = {"text" : "Try >> oeconsol 'd parmlib'"}
+        if 'iplinfo' in entities: data = {"text" : "Try >> oeconsol [d iplinfo]"}
+        elif 'command' in entities: data = {"text" : "Try >> oeconsol [command]"}
+        else: data = {"text" : "Try >> oeconsol [d parmlib]"}
 
     elif intent == "tso":
         data = {"text" : "Try >> tso [-o] [-t] TSO_command"}
     
     else: pass
     return data, confidence
+
--- test_integration/test_skill_nlc2cmd.py
+++ test_integration/test_skill_nlc2cmd.py
@@ -24,7 +24,7 @@ class TestSkillNlc2cmd(ContractSkills):
     def get_commands_expected(self):
         return ['Try >> resource tag-attach --tag-names TAG --resource-name NAME',
                 'Try >> billing account-usage [-d YYYY-MM]',
-                'Try >> OGET 'pathname' mvs_data_set_name(member_name)',
+                'Try >> OGET [pathname] mvs_data_set_name(member_name)',
                 'Try >> obrowse -r xx [file]',
                 'Try >> tar -cjf <archive-file> <directory>',
                 'Try >> tar -xf <archive-file>',
