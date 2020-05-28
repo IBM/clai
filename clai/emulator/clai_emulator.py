6af39ee2af192ca91649947dc33ff06558f64f52
Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>
2020-05-28 09:05:48 -0400

    cache imagetk change
    Signed-off-by: Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>

--- clai/emulator/clai_emulator.py
+++ clai/emulator/clai_emulator.py
@@ -191,15 +191,21 @@ class ClaiEmulator:
 
     def add_refresh_button(self, toolbar):
         path = os.path.dirname(os.path.abspath(__file__))
+
+        # self.refresh_image = tk.PhotoImage(file=f"{path}/refresh.png")
         temp_image = Image.open(f"{path}/refresh.png")
         self.refresh_image = ImageTk.PhotoImage(temp_image)
+
         refresh_button = ttk.Button(toolbar, image=self.refresh_image, command=self.on_refresh_click)
         refresh_button.pack(side=tk.LEFT, padx=2, pady=2)
 
     def add_log_button(self, toolbar):
         path = os.path.dirname(os.path.abspath(__file__))
+
+        # self.log_image = tk.PhotoImage(file=f"{path}/log.png")
         temp_image = Image.open(f"{path}/log.png")
         self.log_image = ImageTk.PhotoImage(temp_image)
+
         log_button = ttk.Button(toolbar, image=self.log_image, command=self.open_log_window)
         log_button.pack(side=tk.LEFT, padx=2, pady=2)
 
