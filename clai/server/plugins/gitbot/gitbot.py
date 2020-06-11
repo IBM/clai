7b8d001f307da635ff5e49f1cf0fb5228e83ffca
Tathagata Chakraborti <tathagata.chakraborti1@ibm.com>
2020-06-11 09:46:00 -0400

    11/06/2020 09:45:58
    modified:   clai/server/plugins/gitbot/gitbot.py + modified:   clai/server/plugins/gitbot/service.py + modified:   clai/server/plugins/gitbot/service.py +

--- clai/server/plugins/gitbot/gitbot.py
+++ clai/server/plugins/gitbot/gitbot.py
@@ -12,19 +12,7 @@ from clai.server.command_message import State, Action, NOOP_COMMAND
 from clai.server.plugins.gitbot.service import Service
 from clai.tools.colorize_console import Colorize
 
-import json
 import os
-
-
-''' globals '''
-_real_path = '/'.join(os.path.realpath(__file__).split('/')[:-1])
-_path_to_config_file = _real_path + '/config.json'
-
-_config = json.loads( open(_path_to_config_file).read() )
-
-_path_to_rasa_model = "{}/rasa/models/{}".format(_real_path, _config["path_to_rasa_saved_model"])
-_rasa_port_number = _config["rasa_port_number"]
-
 
 ''' main gitbot class '''
 class GITBOT(Agent):
@@ -41,31 +29,7 @@ class GITBOT(Agent):
     ''' pre execution processing '''
     def get_next_action(self, state: State) -> Action:
         command = state.command
-        response = self.service(command)
-
-        # action1 = Action(
-        #     suggested_command="git branch | tee {}".format("/Users/tathagata/tmp.log"),
-        #     execute=True,
-        #     description="log: action1",
-        #     confidence=1.0)
-
-        # action2 = Action(
-        #     suggested_command="cat /Users/tathagata/tmp.log",
-        #     execute=True,
-        #     description="log: action2",
-        #     confidence=1.0)
-
-        response_objects = [] 
-
-        for command in response:
-            response_objects.append(Action(
-                suggested_command=command,
-                execute=False, 
-                description=command,
-                confidence=1.0)
-            )
-
-        return response_objects
+        return self.service(command)
 
     ''' pre execution processing '''
     def post_execute(self, state: State) -> Action:
