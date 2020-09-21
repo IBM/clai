d44b767ef2ab8cf682eccf4fb254fd5c974e4fc8
DANFITZ <DANFITZ@ZATA.POK.STGLABS.IBM.COM>
2020-09-21 13:55:11 -0400

    Enable SSL verification in searchlib

--- clai/server/searchlib/providers.py
+++ clai/server/searchlib/providers.py
@@ -117,7 +117,6 @@ class Provider:
         self.__log_json__(request)
         
         session:Session = Session()
-        session.verify = False  # TODO: Fix our certificate verification issue
         response:Response = session.send(request=request.prepare())
         self.__log_json__(response)
         
